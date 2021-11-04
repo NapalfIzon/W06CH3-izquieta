b94350e8c89ffe9aae88b2518938da1be195e936
Napoleon Izquieta <napoleon.izquieta@gmail.com>
2021-11-05 00:12:45 +0100

    :sparkles: Adding GET method functionality for one id in developmentRoutes

--- server/routes/developmentRoutes.js
+++ server/routes/developmentRoutes.js
@@ -11,4 +11,21 @@ developmentRouter.get("/", async (req, res) => {
   debug(chalk.blueBright("Se ha realizado un GET contra BBDD development"));
 });
 
+developmentRouter.get("/:id", async (req, res, next) => {
+  const { id } = req.params;
+  try {
+    const searchedThing = await Development.findById(id);
+    if (searchedThing) {
+      res.json(searchedThing);
+    } else {
+      const error = new Error("Tecnología aprendida no encontrada");
+      error.code = 404;
+      throw error;
+    }
+  } catch (error) {
+    error.code = 400;
+    next(error);
+  }
+});
+
 module.exports = developmentRouter;
