7fe5ef911bf09531d115eb9233cf675932aff498
Napoleon Izquieta <napoleon.izquieta@gmail.com>
2021-11-05 00:11:55 +0100

    :memo: improving code in index.js on serve folder to make the next verbs

--- server/index.js
+++ server/index.js
@@ -3,20 +3,16 @@ const debug = require("debug")("knowledgeThings:indexServer");
 const express = require("express");
 const morgan = require("morgan");
 const mongoDbInitialize = require("../database/index");
+const { errorHandler } = require("./error");
 const developmentRoutes = require("./routes/developmentRoutes");
-const productionRoutes = require("./routes/productionRoutes");
+// const productionRoutes = require("./routes/productionRoutes");
 
 const app = express();
 let portNumber;
-let enviroment;
-let hasAdminRights;
 
 app.use(morgan("dev"));
-
-const initializeServer = ({ port, db, userType }) => {
+const initializeServer = ({ port }) => {
   portNumber = port || 8000;
-  enviroment = db;
-  hasAdminRights = userType;
 
   const server = app.listen(port, () => {
     debug(chalk.yellow(`Escuchando en el puerto ${portNumber}`));
@@ -39,5 +35,7 @@ app.use(morgan("dev"));
 app.use(express.json());
 
 app.use(`/development`, developmentRoutes);
+
+app.use(errorHandler);
 
 module.exports = initializeServer;
